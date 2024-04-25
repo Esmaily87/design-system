69d0a280b4adc97b103ff36c4040b665742c2903
Esmaily Negreiros Peixoto <35602424+Esmaily87@users.noreply.github.com>
2024-04-24 22:54:38 -0300

    Projeto não roda mdx

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -10,7 +10,16 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    
+    '../src/pages/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+
+  
+
+
+  
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-links"),
